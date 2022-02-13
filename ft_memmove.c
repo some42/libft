ae523f925c1d597c428cfaa8c9edb01d52bfb867
somedevv <adriancrmini@gmail.com>
2022-02-13 17:32:42 +0100

    Fixed ft_memmove to pass test

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 20:58:38 by agaliste          #+#    #+#             */
-/*   Updated: 2022/01/24 12:14:35 by agaliste         ###   ########.fr       */
+/*   Updated: 2022/02/13 17:17:59 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,22 @@
 void
 	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned int	i;
+	size_t		i;
+	const char	*src_cpy;
+	char		*dst_cpy;
 
+	src_cpy = (char *)src;
+	dst_cpy = (char *)dst;
 	i = 0;
 	if (!dst && !src)
 		return (NULL);
-	if (src > dst)
+	if (src_cpy < dst_cpy)
 	{
-		while (i < len)
-		{
-			((char *)dst)[i] = ((char *)dst)[i];
-			i++;
-		}
+		while (++i <= len)
+			dst_cpy[len - i] = (char)src_cpy[len - i];
 	}
 	else
-	{
-		i = len;
-		while (i-- > 0)
-			((char *)dst)[i] = ((char *)dst)[i];
-	}
+		while (len-- > 0)
+			*(dst_cpy++) = *(src_cpy++);
 	return (dst);
 }
